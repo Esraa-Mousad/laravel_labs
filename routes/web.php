5465ac0cf99217eb69973f5c8c5007e48f54c288
Esraa-Mousad <esraa98mousad@gmail.com>
2022-02-02 23:07:22 +0200

    login with google and github

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\GitHubController;
+use App\Http\Controllers\GoogleController;
 
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
 
 
 /*
@@ -42,14 +47,47 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/auth/redirect', function () {
-    return Socialite::driver('github')->redirect();
-})->name('auth.github');
 
-Route::get('/auth/callback', function () {
-    $user = Socialite::driver('github')->user();
-    dd($user);
-    // return redirect()->route('posts.index');
+Route::get('auth/github', [GitHubController::class, 'gitRedirect']);
+Route::get('auth/github/callback', [GitHubController::class, 'gitCallback']);
 
-    // $user->token
-});
+Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
+Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+
+ 
+
+// Route::get('/auth/redirect', function () {
+//     return Socialite::driver('github')->stateless()->redirect();
+// })->name('auth.github');
+
+// Route::get('/auth/callback', function () {
+//     $user = Socialite::driver('github')->user();
+//     dd($user);
+//     // return redirect()->route('posts.index');
+
+//     // $user->token
+// });
+
+// Route::get('/auth/github/callback', function () {
+//     $githubUser = Socialite::driver('github')->stateless()->user();
+    
+//     $user = User::where('github_id' , $githubUser->id)->first();
+    
+//     if($user){
+//     $user->update([
+//     'github_token' => $githubUser->token,
+//     'github_refresh_token' => $githubUser->refreshToken,
+//     ]);
+//     }else{
+//     $user=User::create([
+//     'name' => $githubUser->name,
+//     'email' => $githubUser->email,
+//     'github_id' => $githubUser->id,
+//     'github_token' => $githubUser->token,
+//     'github_refresh_token' => $githubUser->refreshToken,
+//     ]);
+//     }
+    
+//     Auth::login($user ,true);
+//     return redirect()->route('posts.index');
+// });
